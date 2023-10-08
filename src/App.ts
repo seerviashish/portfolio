c6736b31a70b25155353ad1febd305f6d9ead284
Ashish Chaudhary <seerviashish17@gmail.com>
2023-10-08 18:11:17 +0530

    feat: 🎸 tweakpane
    add a cube folder

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,6 @@
 import { Color, PerspectiveCamera, Renderer, Scene, WebGLRenderer } from 'three'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { Pane } from 'tweakpane'
 import Cube from './objects/Cube'
 export interface IApp {
   init: () => Promise<void>
@@ -10,6 +12,16 @@ export default class App implements IApp {
   private renderer: Renderer
   private scene: Scene
   private cube?: Cube
+  private stats?: Stats
+  private pane?: Pane
+  private cubeParams: { x: number; y: number; z: number; dx: number; dy: number; dz: number } = {
+    x: 0,
+    y: 0,
+    z: -5,
+    dx: 0.01,
+    dy: 0.01,
+    dz: 0,
+  }
 
   public static getInstance(): App {
     if (!App.instance) {
@@ -32,25 +44,63 @@ export default class App implements IApp {
     this.render()
   }
 
-  animate = () => {
+  animate = (): void => {
     requestAnimationFrame(this.animate)
     this.renderer.render(this.scene, this.camera)
+    this.updateStats()
     this.render()
+  }
+
+  isProduction = (): boolean => {
+    return process.env.NODE_ENV === 'production'
+  }
+
+  addStats = (): void => {
+    if (this.isProduction()) return
+    this.stats = new Stats()
+    document.body.appendChild(this.stats.dom)
+  }
+
+  updateStats = () => {
+    if (this.isProduction()) return
+    if (!this.stats) return
+    this.stats.update()
+  }
+
+  addPane = (): void => {
+    if (this.isProduction()) return
+    this.pane = new Pane()
+  }
+
+  addCubePane = (): void => {
+    if (!this.pane) return
+    const cubePane = this.pane.addFolder({
+      title: 'Cube',
+    })
+    cubePane.addBinding(this.cubeParams, 'x')
+    cubePane.addBinding(this.cubeParams, 'y')
+    cubePane.addBinding(this.cubeParams, 'z')
+    cubePane.addBinding(this.cubeParams, 'dx')
+    cubePane.addBinding(this.cubeParams, 'dy')
+    cubePane.addBinding(this.cubeParams, 'dz')
   }
 
   async init() {
     this.scene.background = new Color(0x000000)
     document.body.appendChild(this.renderer.domElement)
+    this.addStats()
+    this.addPane()
     window.addEventListener('resize', this.handleOnWindowResize)
     this.animate()
     this.initCube()
   }
 
   private initCube() {
-    this.cube = new Cube([1, 1, 1], { color: 0x00ff00 })
+    this.cube = new Cube([1, 1, 1], { color: 0x00ff00, wireframe: true })
     this.scene.add(this.cube.getCube())
     const cubeMesh = this.cube.getCube()
     cubeMesh.position.z = -5
+    this.addCubePane()
   }
 
   private renderCube() {
@@ -58,8 +108,12 @@ export default class App implements IApp {
       return
     }
     const cubeMesh = this.cube.getCube()
-    cubeMesh.rotation.x += 0.01
-    cubeMesh.rotation.y += 0.01
+    cubeMesh.position.x = this.cubeParams.x
+    cubeMesh.position.y = this.cubeParams.y
+    cubeMesh.position.z = this.cubeParams.z
+    cubeMesh.rotation.x += this.cubeParams.dx
+    cubeMesh.rotation.y += this.cubeParams.dy
+    cubeMesh.rotation.z += this.cubeParams.dz
   }
 
   render = () => {
--- src/objects/Cube.ts
+++ src/objects/Cube.ts
@@ -8,7 +8,10 @@ export default class Cube implements ICube {
   private geometry: BoxGeometry
   private material: MeshBasicMaterial
   private mesh: Mesh
-  constructor(geometry: [number, number, number], meshParams?: { color: ColorRepresentation }) {
+  constructor(
+    geometry: [number, number, number],
+    meshParams?: { color: ColorRepresentation; wireframe?: boolean },
+  ) {
     this.geometry = new BoxGeometry(...geometry)
     this.material = new MeshBasicMaterial({ ...meshParams })
     this.mesh = new Mesh(this.geometry, this.material)
